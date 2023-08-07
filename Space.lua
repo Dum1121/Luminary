0b4ee4cb51279172a1daed6c3eed202d551425f6
Dum <91747868+Dum121@users.noreply.github.com>
2023-08-07 18:30:12 +0700

    Update Space.lua

--- Space.lua
+++ Space.lua
@@ -2,7 +2,7 @@ local Id = {
 	PetXId1 = 6284583030,
 }
 for _,v in pairs(Id) do
-	if game.PlaceId == v then
-		--script
+	if v == 6284583030 then
+		loadstring(game:HttpGet(('https://raw.githubusercontent.com/Dum121/Luminary/main/Pet%20Simulator%20X.lua')))()
 	end
 end
