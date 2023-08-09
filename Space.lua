0d4713d222d635611a1b5b147a7aa962a1a096d4
Dum <91747868+Dum121@users.noreply.github.com>
2023-08-09 22:54:17 +0700

    Update Space.lua

--- Space.lua
+++ Space.lua
@@ -3,6 +3,7 @@ local Id = {
 	OPL = 3237168,
 }
 local logo = "rbxassetid://14098292415"
+local PlaceId == game.PlaceId
 local Name = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
 function Noti(Title,Text,Duration,Icon)
 game.StarterGui:SetCore("SendNotification", {
@@ -12,13 +13,13 @@ Icon = Icon or logo; -- the image if u want.
 Duration = Duration or 10; -- how long the notification should in secounds
 })end
 for _,v in pairs(Id) do
-	if v == 6284583030 then
+	if v == PlaceId then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Dum121/LuminaryExtra/main/PetSimulatorXBypass.lua",true))()
 Noti("Luminary Space\nPet Simulator X\n","Waiting Bypass\nAnti Cheat https://discord.gg/GJNn6TTX5K",5)
 wait(5)
 		Noti("Luminary Space\nPet Simulator X\n","Kaitun Diamond - Fruit - Mystery https://discord.gg/GJNn6TTX5K",math.huge)
 		loadstring(game:HttpGet('https://raw.githubusercontent.com/Dum121/Luminary/main/Pet%20Simulator%20X.lua'))()
-	elseif v == 3237168 and getgenv().Luminary == "Luminary~Space" then
+	elseif v == PlaceId and getgenv().Luminary == "Luminary~Space" then
 		Noti()
 		wait(2)
 		loadstring(game:HttpGet('https://raw.githubusercontent.com/Dum121/Luminary/main/One%20Piece%20Legendary.lua'))()
