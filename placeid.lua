f57d6af74d66feec2f82729d4bf93b131c4dd4ca
Dum <91747868+Dum121@users.noreply.github.com>
2023-08-15 23:28:49 +0700

    Update placeid.lua

--- placeid.lua
+++ placeid.lua
@@ -2,7 +2,7 @@ getgenv().getextra = {
   [6284583030] = "https://raw.githubusercontent.com/Dum121/LuminaryExtra/main/Pet%20Simulator%20X%20Extra/PetSimulatorXBypass.lua",
 }
 getgenv().getid = {
-  [6284583030] = "https://raw.githubusercontent.com/Dum121/Luminary/main/File/Pet%20Simulator%20X.lua",
-  [3237168] = "https://raw.githubusercontent.com/Dum121/Luminary/main/File/One%20Piece%20Legendary.lua",
+  [6284583030] = "https://raw.githubusercontent.com/Dum121/Luminary/main/Scripts/Pet%20Simulator%20X.lua",
+  [3237168] = "https://raw.githubusercontent.com/Dum121/Luminary/main/Scripts/One%20Piece%20Legendary.lua",
   [5732973455] = "https://raw.githubusercontent.com/Dum121/Luminary/main/Scripts/Hours.lua",
 }
